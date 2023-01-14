d37e1ef58c2b2d0240cfc4b77dfc31b3e588fec5
Vertigofy <69199577+Vertiverti@users.noreply.github.com>
2023-01-14 11:51:59 +0800

    aaaa

--- Content/UI/DialogueBox.gd
+++ Content/UI/DialogueBox.gd
@@ -3,8 +3,10 @@ class_name DialogueBox
 
 var t
 var dialogue
+var dcontainer = preload("res://Content/UI/DialogueContainer.tscn")
 
 func _init(text : String) -> void:
+	var d = dcontainer.instance(); d.rect_position = Vector2(-20,-17); add_child(d)
 	t = Timer.new(); t.connect("timeout", self, "append_new_letter")
 	t.wait_time = 0.05; t.one_shot = true; t.start()
 	dialogue = text
