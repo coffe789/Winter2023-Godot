059393e26782d6521ddc4bf9b5e44d4839c41efe
Vertigofy <69199577+Vertiverti@users.noreply.github.com>
2023-01-14 15:03:54 +0800

    kity new sprite

--- Content/UI/DialogueBox.gd
+++ Content/UI/DialogueBox.gd
@@ -4,11 +4,12 @@ class_name DialogueBox
 var t
 var dialogue
 var dcontainer = preload("res://Content/UI/DialogueContainer.tscn")
+var text_complete
 
 func _init(text : String) -> void:
 	var d = dcontainer.instance(); d.rect_position = Vector2(-20,-17); add_child(d)
 	t = Timer.new(); t.connect("timeout", self, "append_new_letter")
-	t.wait_time = 0.05; add_child(t); t.one_shot = true
+	t.wait_time = 0.1; add_child(t); t.one_shot = true
 	dialogue = text
 
 func _ready():
@@ -18,3 +19,12 @@ func append_new_letter() -> void:
 	if $VBoxContainer/Label.text.length() < dialogue.length():
 		$VBoxContainer/Label.text += dialogue[$VBoxContainer/Label.text.length()]
 		t.start()
+	else:
+		text_complete = true
+
+func _input(event):
+	if event.is_action_pressed("ui_accept"):
+		if text_complete:
+			queue_free()
+		else:
+			t.wait_time = 0.01
