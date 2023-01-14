54a13344ed8a9e7e6f7ffe5d5e50bb9337d5ad39
Coffe <jonathan_davey@outlook.com>
2023-01-14 14:52:56 +0800

    Kity can talk :3

--- Content/UI/DialogueBox.gd
+++ Content/UI/DialogueBox.gd
@@ -8,10 +8,13 @@ var dcontainer = preload("res://Content/UI/DialogueContainer.tscn")
 func _init(text : String) -> void:
 	var d = dcontainer.instance(); d.rect_position = Vector2(-20,-17); add_child(d)
 	t = Timer.new(); t.connect("timeout", self, "append_new_letter")
-	t.wait_time = 0.05; t.one_shot = true; t.start()
+	t.wait_time = 0.05; add_child(t); t.one_shot = true
 	dialogue = text
 
+func _ready():
+	t.start()
+
 func append_new_letter() -> void:
-	if Label.text.length() < dialogue.length():
-		Label.text.append(dialogue[Label.text.length - 1])
+	if $VBoxContainer/Label.text.length() < dialogue.length():
+		$VBoxContainer/Label.text += dialogue[$VBoxContainer/Label.text.length()]
 		t.start()
