f2be13ef6b2a7c60bc5259120c82b862fe2ac4a4
Coffe <jonathan_davey@outlook.com>
2023-01-15 16:17:04 +0800

    New level

--- Content/LevelManager.gd
+++ Content/LevelManager.gd
@@ -8,6 +8,7 @@ var levels = [
 	preload("res://Content/Level/Level1.tscn"),
 	preload("res://Content/Level/LevelWaterIntro.tscn"),
 	preload("res://Content/Level/LevelWaterIntro2.tscn"),
+	preload("res://Content/Level/LevelNearby.tscn"),
 	preload("res://Content/Level/LevelWaterTrap.tscn"),
 ]
 
